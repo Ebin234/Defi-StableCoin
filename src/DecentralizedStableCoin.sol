718f6b19a65117b63d0f5d819bdadc0b47579c9c
Ebin234 <ebinyesudas234@gmail.com>
2025-12-31 11:12:09 +0000

    Added mint and burn functions

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -15,5 +15,36 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  */
 
 contract DecentralizedStableCoin is ERC20Burnable,Ownable{
+    /** ERRORS */
+    error DecentralizedStableCoin__BurnAmountMustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsUserBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    /** FUNCTIONS */
+
     constructor(address initialOwner) ERC20("DecentralizedStableCoin","DSC") Ownable(initialOwner){}
+
+    function burn(uint256 _amount) public override onlyOwner{
+        uint256 userBalance = balanceOf(msg.sender);
+        if(_amount <= 0){
+            revert DecentralizedStableCoin__BurnAmountMustBeMoreThanZero();
+        }
+        if(userBalance < _amount){
+            revert DecentralizedStableCoin__BurnAmountExceedsUserBalance();
+        }
+
+        super.burn(_amount);
+    }
+
+    function mint(address _to,uint256 _amount) external onlyOwner returns(bool){
+        if(_to == address(0)){
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if(_amount <= 0){
+            revert DecentralizedStableCoin__BurnAmountMustBeMoreThanZero();
+        }
+
+        _mint(_to,_amount);
+        return true;
+    }
 }
